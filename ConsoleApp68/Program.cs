15fd76b0a32c2ebfa8e1fa67179fc9824c4e81bf
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-09 17:45:20 +0330

    Add files via upload

---
+++ ConsoleApp68/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ismale = true;
+            bool istall = true;
+
+            if (ismale && istall)
+            {
+                Console.WriteLine("you are tall male");
+            }
+            else if (ismale && !istall)
+            {
+                Console.WriteLine("you are short male");
+            }
+            else if (!ismale && istall)
+            {
+                Console.WriteLine("you are note a male but you are tall");
+            }
+            else
+            {
+                Console.WriteLine("you are either not male or not tall");
+            }
+            Console.ReadLine();
+        }
+    }
+}
